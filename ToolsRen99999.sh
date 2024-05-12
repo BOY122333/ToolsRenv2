882b60b1616f1efcb38daf6475090d159cd64d8e
BOY122333 <pribadisaya10101010@gmail.com>
2024-05-12 12:22:03 +0700

    Update ToolsRen99999.sh

--- ToolsRen99999.sh
+++ ToolsRen99999.sh
@@ -1,6 +1,16 @@
+#Linux999
+#Debian
+#MauNgapainGblk?
+#DiRecodeGakBakalJadiinLuMastah
+#DiGantiAuthorGakBakalJadiinLuMaster
+#MikirMakeOtakGblk
+#KontolKaloMauReedit/Recode 
+
+
 sleep 1
 clear
-sleep 1
+sleep 2
+clear
 echo -e "  ████████╗░█████╗░░█████╗░██╗░░░░░░██████╗██████╗░███████╗███╗░░██╗"
 echo -e "  ╚══██╔══╝██╔══██╗██╔══██╗██║░░░░░██╔════╝██╔══██╗██╔════╝████╗░██║"
 echo -e "  ░░░██║░░░██║░░██║██║░░██║██║░░░░░╚█████╗░██████╔╝█████╗░░██╔██╗██║"
@@ -24,11 +34,20 @@ echo -e "               ||5.spam otp no stop v5                  ||"
 echo -e "               ||6.spam otp no stop v6                  ||"
 echo -e "               ||7.spam otp no stop v7                  ||"
 echo -e "               || ▀▀ ▀▀ ▀▀ ▀▀ ▀▀ ▀▀ ▀▀▀▀ ▀▀ ▀▀ ▀▀ ▀▀ ▀▀ ||"
-sleep 2
+echo -e "               ||          MENU PHISING & HACKING       ||"
 echo -e "               || ▀▀▀ ▀▀ ▀▀ ▀▀ ▀▀ ▀▀ ▀▀▀▀ ▀▀ ▀▀ ▀▀ ▀▀ ▀ ||"
-echo -e "               ||                MENU SPAM              ||"
-echo -e "               || ▀▀▀ ▀▀ ▀▀ ▀▀ ▀▀ ▀▀ ▀▀▀▀ ▀▀ ▀▀ ▀▀ ▀▀ ▀ ||"
-echo
+echo -e "               ||1.HACK KAMERA                          ||"
+echo -e "               ||2.HACK SOSMED                          ||"
+echo -e "               ||3.HACK CCTV                            ||"
+echo -e "               ||4.PANDORA BY SANZ                      ||"
+echo -e "               ||5.Sploit-Malicious                     ||"
+echo -e "               ||6.diejoubu                             ||"
+echo -e "               ||7.Lazymux                              ||"
+echo -e "               ||8.XPHISHER                             ||"
+echo -e "               ||9.OSGPT                                ||"
+echo -e "               ||10.NETHUNTER M                         ||"
+echo -e "               || ▀▀ ▀▀ ▀▀ ▀▀ ▀▀ ▀▀ ▀▀▀▀ ▀▀ ▀▀ ▀▀ ▀▀ ▀▀ ||"
+ech
 read -p "               PILIH MENU:" ren9999
 if [ $ren9999 == 1 ]
 then
@@ -37,7 +56,7 @@ sleep 1
 pkg install python
 pkg install  git
  python -m pip install requests
- git clone https://github.com/RozhBasXYZ/OTP
+ git clone https://github.com/RozhBasXYZ/OTP.git
  cd OTP
  python spam.py
 fi
@@ -50,7 +69,7 @@ pkg update && pkg upgrade
 pkg install nano
 pkg install git
 pkg install python
-git clone https://github.com/AmmarrBN/Tools-WhatsApp
+git clone https://github.com/AmmarrBN/Tools-WhatsApp.git
 bash install.sh
 cd Tools-WhatsApp
 python main.py
@@ -63,7 +82,7 @@ sleep 1
 pkg install python
 pkg install git
 python3 -m pip install requests
-git clone https://github.com/krypton-byte/SpamWa
+git clone https://github.com/krypton-byte/SpamWa.git
 cd SpamWa
 python3 spam.py
 fi
@@ -92,7 +111,7 @@ pkg install python-pip
 pkg install git
 pip install requests
 pip install rich
-git clone https://github.com/TZSEC7/TZspam
+git clone https://github.com/TZSEC7/TZspam.git
 cd TZspam
 python TZsec7.py
 Spam
@@ -108,7 +127,7 @@ pkg install bash curl wget jq
 pkg install python python-pip
 pkg install make clang
 pkg install git
-git clone https://github.com/Sxp-ID/Brutal-W4
+git clone https://github.com/Sxp-ID/Brutal-W4.git
  cd Brutal-W4
 git pull
 make install
@@ -128,3 +147,118 @@ pip install -r install.txt
 python brutal.py
 fi
 
+if [ $ren9999 == 1 ]
+then
+clear
+sleep 2
+git clone https://github.com/jasut1n/WebCamPhishing.git
+cd WebCamPhishing
+chmod +x webcamphish.sh
+./webcamphish.sh
+fi
+
+if [ $ren9999 == 2 ]
+then
+clear
+sleep 2
+git clone --depth=1 https://github.com/htr-tech/zphisher.git
+cd zphisher
+bash zphisher.sh
+fi
+
+if [ $ren9999 == 3 ]
+then
+clear
+sleep 2
+pkg update && pkg upgrade 
+git clone https://github.com/mohammadmahdi-termux/hackCCTV.git
+cd hackCCTV 
+python3 hackcctv.py
+fi
+
+if [ $ren9999 == 4 ]
+then
+clear
+sleep 2
+apt update && apt upgrade && pkg update && pkg upgrade
+pkg install curl
+pkg install wget
+pkg install python3
+pkg install git
+pkg install curl
+git clone https://github.com/MrSanZz/pandora
+cd pandora
+pip3 install -r requirements.txt
+python3 pandora.py
+fi
+
+if [ $ren9999 == 5 ]
+then
+clear
+sleep 2
+apt-get install git
+apt-get install python2 ruby
+gem install lolcat
+git clone https://github.com/TheSploit/Sploit-Malicious.git
+cd Sploit-Malicious
+pip2 install -r requirements.txt
+python2 malicious.py
+fi
+
+if [ $ren9999 == 6 ]
+then
+clear
+sleep 2
+pkg upgrade && pkg update
+pkg install php
+pkg install git
+gitclone https://github.com/alintamvanz/diejoubu.git
+cd diejoubu
+cd v1.2
+php daijobu.php
+fi
+
+if [ $ren9999 == 7 ]
+then
+clear
+sleep 2
+pkg update && upgrade
+pkg install git
+pkg install python
+pkg install python2
+git clone https://github.com/Gameye98/Lazymux.git
+cd Lazymux
+python lazymux.py
+fi
+
+if [ $ren9999 == 8 ]
+then
+clear
+sleep 2
+git clone https://github.com/XPH4N70M/XPHISHER.git
+cd XPHISHER 
+bash xphisher.sh
+fi
+
+if [ $ren9999 == 9 ]
+then
+clear
+sleep 2
+git clone https://github.com/Ravi-Teja-konda/OSGPT.git 
+cd OSGPT
+pip install -r requirements.txt
+python3 main.py
+fi
+
+if [ $ren9999 == 10 ]
+then
+clear
+sleep 2
+pkg install root-repo 
+ pkg install x11-repo
+ apt update && apt upgrade -y
+ apt install wget -y 
+ wget https://raw.githubusercontent.com/shahinuralamin/Technical_Shahinur_Al-Amin/main/install-nethunter-modified 
+chmod +x install-nethunter-modified
+./install-nethunter-modified
+fi

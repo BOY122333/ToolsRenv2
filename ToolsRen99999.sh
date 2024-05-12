a873d35241fd9f71c9815db903e341179643b0f6
BOY122333 <pribadisaya10101010@gmail.com>
2024-05-12 11:26:26 +0700

    Update ToolsRen99999.sh

--- ToolsRen99999.sh
+++ ToolsRen99999.sh
@@ -10,7 +10,121 @@ echo -e "  ░░░╚═╝░░░░╚════╝░░╚════
 echo -e " "
 echo
 echo -e "                          By: Ren99999 ☕🗿"
-echo -e "                ▀▀ ▀▀ ▀▀ ▀▀ ▀▀ ▀▀ ▀▀▀▀ ▀▀ ▀▀ ▀▀ ▀▀ ▀▀ ▀▀"
-echo -e "               ||        𝐖𝐄𝐋𝐂𝐎𝐌𝐄 𝐓𝐎 𝐓𝐎𝐎𝐋𝐒𝐑𝐄𝐍             ||"
-echo -e "               ||                                            ||"
-echo -e "                ▀▀ ▀▀ ▀▀ ▀▀ ▀▀ ▀▀ ▀▀▀▀ ▀▀ ▀▀ ▀▀ ▀▀ ▀▀ ▀▀||"
+echo -e "               ||▀▀ ▀▀ ▀▀ ▀▀ ▀▀ ▀▀ ▀▀▀▀ ▀▀ ▀▀ ▀▀ ▀▀ ▀▀ ▀||"
+echo -e "               ||        𝐖𝐄𝐋𝐂𝐎𝐌𝐄 𝐓𝐎 𝐓𝐎𝐎𝐋𝐒𝐑𝐄𝐍         ||"
+echo -e "               ||                                       ||"
+echo -e "               || ▀▀▀ ▀▀ ▀▀ ▀▀ ▀▀ ▀▀ ▀▀▀▀ ▀▀ ▀▀ ▀▀ ▀▀ ▀ ||"
+echo -e "               ||                MENU SPAM              ||"
+echo -e "               || ▀▀▀ ▀▀ ▀▀ ▀▀ ▀▀ ▀▀ ▀▀▀▀ ▀▀ ▀▀ ▀▀ ▀▀ ▀ ||"
+echo -e "               ||1.spam otp no stop v1                  ||"
+echo -e "               ||2.spam otp no stop v2                  ||"
+echo -e "               ||3.spam otp no stop v3                  ||"
+echo -e "               ||4.spam otp no stop v4                  ||"
+echo -e "               ||5.spam otp no stop v5                  ||"
+echo -e "               ||6.spam otp no stop v6                  ||"
+echo -e "               ||7.spam otp no stop v7                  ||"
+echo -e "               || ▀▀ ▀▀ ▀▀ ▀▀ ▀▀ ▀▀ ▀▀▀▀ ▀▀ ▀▀ ▀▀ ▀▀ ▀▀ ||"
+sleep 2
+echo -e "               || ▀▀▀ ▀▀ ▀▀ ▀▀ ▀▀ ▀▀ ▀▀▀▀ ▀▀ ▀▀ ▀▀ ▀▀ ▀ ||"
+echo -e "               ||                MENU SPAM              ||"
+echo -e "               || ▀▀▀ ▀▀ ▀▀ ▀▀ ▀▀ ▀▀ ▀▀▀▀ ▀▀ ▀▀ ▀▀ ▀▀ ▀ ||"
+echo
+read -p "               PILIH MENU:" ren9999
+if [ $ren9999 == 1 ]
+then
+clear
+sleep 1
+pkg install python
+pkg install  git
+ python -m pip install requests
+ git clone https://github.com/RozhBasXYZ/OTP
+ cd OTP
+ python spam.py
+fi
+
+if [ $ren9999 == 2 ]
+then
+clear
+sleep 1
+pkg update && pkg upgrade
+pkg install nano
+pkg install git
+pkg install python
+git clone https://github.com/AmmarrBN/Tools-WhatsApp
+bash install.sh
+cd Tools-WhatsApp
+python main.py
+fi
+
+if [ $ren9999 == 3 ]
+then
+clear
+sleep 1
+pkg install python
+pkg install git
+python3 -m pip install requests
+git clone https://github.com/krypton-byte/SpamWa
+cd SpamWa
+python3 spam.py
+fi
+
+if [ $ren9999 == 4 ]
+then
+clear
+sleep1
+apt update && apt upgrade
+apt install git -y
+apt install python3-pip
+git clone https://github.com/FatihArridho/brutal.git
+cd brutal
+pip3 install -r install.txt
+python3 brutal.py
+fi
+
+if [ $ren9999 == 5 ]
+then
+clear
+sleep 1
+pkg update
+pkg upgrage
+pkg install python
+pkg install python-pip
+pkg install git
+pip install requests
+pip install rich
+git clone https://github.com/TZSEC7/TZspam
+cd TZspam
+python TZsec7.py
+Spam
+fi
+
+if [ $ren9999 == 6 ]
+then
+clear
+sleep 1
+cd $HOME
+apt update && apt upgrade
+pkg install bash curl wget jq
+pkg install python python-pip
+pkg install make clang
+pkg install git
+git clone https://github.com/Sxp-ID/Brutal-W4
+ cd Brutal-W4
+git pull
+make install
+./main
+fi
+
+if [ $ren9999 == 7 ]
+then
+clear
+sleep 1
+apt update -y && apt upgrade -y
+apt install git -y
+apt install python -y
+git clone https://github.com/FatihArridho/brutal.git
+cd brutal
+pip install -r install.txt
+python brutal.py
+fi
+

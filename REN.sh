491de0bfe87dec9f4e0be9f6b80205823e07d8cc
BOY122333 <pribadisaya10101010@gmail.com>
2024-05-21 07:42:06 +0700

    Add files via upload

---
+++ REN.sh
@@ -0,0 +1,598 @@
+xdg-open "https://wa.me/+6289519450908?text=```Bang gw Mau beli Script ToolsRenv1```"
+clear
+verify_credentials() {
+local expected_username="REN"
+local expected_password="99999"
+while true; do
+read -p "Username: " username
+read -s -p "Password: " password
+echo
+if [ "$username" == "$expected_username" ] && [ "$password" == "$expected_password" ]; then
+echo "Login berhasil!"
+break
+else
+echo "Username atau password salah. Silakan coba lagi."
+fi
+done
+}
+verify_credentials
+sleep 2
+clear
+sleep 1
+echo -e   "\e[32m████████╗░█████╗░░█████╗░██╗░░░░░░██████╗██████╗░███████╗███╗░░██╗"
+echo -e   "\e[32m╚══██╔══╝██╔══██╗██╔══██╗██║░░░░░██╔════╝██╔══██╗██╔════╝████╗░██║"
+echo -e   "\e[32m░░░██║░░░██║░░██║██║░░██║██║░░░░░╚█████╗░██████╔╝█████╗░░██╔██╗██║"
+echo -e   "\e[32m░░░██║░░░██║░░██║██║░░██║██║░░░░░░╚═══██╗██╔══██╗██╔══╝░░██║╚████║"
+echo -e   "\e[32m░░░██║░░░╚█████╔╝╚█████╔╝███████╗██████╔╝██║░░██║███████╗██║░╚███║"
+echo -e   "\e[32m░░░╚═╝░░░░╚════╝░░╚════╝░╚══════╝╚═════╝░╚═╝░░╚═╝╚══════╝╚═╝░░╚══╝"
+echo
+echo
+echo -e "                       𝚅𝙴𝚁𝚂𝙸 ��𝙿��𝙰��𝙴 𝚃𝙴𝚁𝙱𝙰𝚁𝚄"
+echo -e "                         𝐁𝐲: 𝐑𝐄𝐍𝟗𝟗𝟗𝟗 ☕🗿"
+echo -e "               ||=======================================||"
+echo -e "               ||        WELCOME TO TOOLSRENv1          ||"
+echo -e "               ||=======================================||"
+echo -e "               ||               MENU SPAM               ||"
+echo -e "               ||=======================================||"
+echo -e "               ||1.spam otp no stop V1                  ||"
+echo -e "               ||2.spam otp no stop V2                  ||"
+echo -e "               ||3.spam otp no stop V3                  ||"
+echo -e "               ||4.spam otp no stop V4                  ||"
+echo -e "               ||5.spam otp no stop V5                  ||"
+echo -e "               ||6.spam otp no stop V6                  ||"
+echo -e "               ||7.spam otp no stop V7                  ||"
+echo -e "               ||8.spam otp no stop V8                  ||"
+echo -e "               ||9.spam otp no stop V9                  ||"
+echo -e "               ||10.spam otp no stop V10                ||"
+echo -e "               ||11.spam otp no stop V11                ||"
+echo -e "               ||12.spam otp no stop V12                ||"
+echo -e "               ||13.spam otp no stop V13                ||"
+echo -e "               ||14.spam otp no stop V14                ||"
+echo -e "               ||15.spam otp no stop V15                ||"
+echo -e "               ||=======================================||"
+echo -e "               ||         MENU PHISING & HACKING        ||"
+echo -e "               ||=======================================||"
+echo -e "               ||16..HACK KAMERA                        ||"
+echo -e "               ||17.HACK SOSMED                         ||"
+echo -e "               ||18.HACK CCTV                           ||"
+echo -e "               ||19.PANDORA BY SANZ                     ||"
+echo -e "               ||20.Sploit-Malicious                    ||"
+echo -e "               ||21.diejoubu                            ||"
+echo -e "               ||22.Lazymux                             ||"
+echo -e "               ||23.XPHISHER                            ||"
+echo -e "               ||24.OSGPT                               ||"
+echo -e "               ||25.NETHUNTER M                         ||"
+echo -e "               ||26.GABUT                               ||"
+echo -e "               ||27.sqlmap                              ||"
+echo -e "               ||28.TOOLSINSTALLERv3                    ||"
+echo -e "               ||29.Crack fb                            ||"
+echo -e "               ||30.Crack ig                            ||"
+echo -e "               ||31.instabot                            ||"
+echo -e "               ||32.caping                              ||"
+echo -e "               ||33.Tools ABD                           ||"
+echo -e "               ||34.VBUG                                ||"
+echo -e "               ||=======================================||"
+echo -e "               ||                MENU DDOS              ||"
+echo -e "               ||=======================================||"
+echo -e "               ||35.DDOS V1                             ||"
+echo -e "               ||36.DDOS V2                             ||"
+echo -e "               ||37.DDOS V3                             ||"
+echo -e "               ||38.DDOS V4 <off>                       ||"
+echo -e "               ||39.DDOS V5                             ||"
+echo -e "               ||40.DDOS V6                             ||"
+echo -e "               ||41.DDOS V7                             ||"
+echo -e "               ||=======================================||"
+echo -e "               ||                MENU DLL               ||"
+echo -e "               ||=======================================||"
+echo -e "               ||UPDATE                                 ||"
+echo -e "               ||FOLLOW                                 ||"
+echo -e "               ||{ bug }                                ||"
+echo -e "               ||{0} EXIT                               ||"
+echo -e "               ||=======================================||"
+echo
+read -p "               PILIH MENU:" ren9999
+if [ $ren9999 == 1 ]
+then
+clear
+sleep 1
+pkg install python
+xdg-open "git clone https://tinyurl.com/uppdatesnew"
+fi
+if [ $ren9999 == 2 ]
+then
+clear
+sleep 1
+pkg update && pkg upgrade
+pkg install nano
+pkg install git
+pkg install python
+git clone https://github.com/AmmarrBN/Tools-WhatsApp.git
+bash install.sh
+cd Tools-WhatsApp
+python main.py
+fi
+if [ $ren9999 == 3 ]
+then
+clear
+sleep 1
+xdg-open "git clone https://tinyurl.com/uppdatesnew"
+fi
+if [ $ren9999 == 4 ]
+then
+clear
+sleep1
+apt update && apt upgrade
+apt install git -y
+apt install python3-pip
+git clone https://github.com/FatihArridho/brutal.git
+cd brutal
+pip3 install -r install.txt
+python3 brutal.py
+fi
+if [ $ren9999 == 5 ]
+then
+clear
+sleep 1
+pkg update
+pkg upgrage
+pkg install python
+pkg install python-pip
+pkg install git
+pip install requests
+pip install rich
+git clone https://github.com/TZSEC7/TZspam.git
+cd TZspam
+python TZsec7.py
+Spam
+fi
+if [ $ren9999 == 6 ]
+then
+clear
+sleep 1
+cd $HOME
+apt update && apt upgrade
+pkg install bash curl wget jq
+pkg install python python-pip
+pkg install make clang
+pkg install git
+git clone https://github.com/Sxp-ID/Brutal-W4.git
+cd Brutal-W4
+git pull
+make install
+./main
+fi
+if [ $ren9999 == 7 ]
+then
+clear
+sleep 1
+apt update -y && apt upgrade -y
+apt install git -y
+apt install python -y
+git clone https://github.com/FatihArridho/brutal.git
+cd brutal
+pip install -r install.txt
+python brutal.py
+fi
+if [ $ren9999 == 8 ]
+then
+clear
+sleep 1
+apt update && apt upgrade
+apt install python2
+apt install php
+apt install git
+git clone https://github.com/joss24242/SpamSms.git
+cd SpamSms
+python2 mantan.py
+fi
+if [ $ren9999 == 9 ]
+then
+clear
+sleep 1
+pkg update && pkg upgrade
+pkg install python2
+pkg install php
+pkg install php
+git clone https://github.com/siputra12/prank.git
+cd prank
+ls
+php wa.php
+fi
+if [ $ren9999 == 10 ]
+then
+clear
+sleep 1
+apt upgrade && apt update
+apt install git
+git clone https://github.com/mrmsdv/sdvspam.git
+cd sdvspam
+sh install.sh
+sh sdv.sh
+fi
+if [ $ren9999 == 11 ]
+then
+clear
+sleep 1
+python main.py
+git clone https://github.com/rickyfazaa/MySPAMBot-OTP
+cd MySPAMBot-OTP
+python main.py
+fi
+if [ $ren9999 == 12 ]
+then
+clear
+sleep 1
+pkg update && pkg upgrade
+pkg install nano
+pkg install git
+pkg install python
+git clone https://github.com/AmmarrBN/Tools-WhatsApp
+bash install.sh
+cd Tools-WhatsApp
+python main.py
+fi
+if [ $ren9999 == 13 ]
+then
+clear
+sleep 1
+pkg install nodejs && git
+git clone https://github.com/ibnusyawall/spam-wa
+npm i
+node index
+fi
+if [ $ren9999 == 14 ]
+then
+clear
+sleep 1
+git clone https://github.com/RayhanZuck/BrutalSpam
+cd BrutalSpam
+python spam.py
+fi
+if [ $ren9999 == 15 ]
+then
+clear
+sleep 1
+pkg install python
+pip install requestS
+git clone https://github.com/BOY122333/spamCall.git
+cd spamCall
+bash call.sh
+fi
+if [ $ren9999 == 16 ]
+then
+clear
+sleep 1
+git clone https://github.com/jasut1n/WebCamPhishing.git
+cd WebCamPhishing
+chmod +x webcamphish.sh
+./webcamphish.sh
+fi
+if [ $ren9999 == 17 ]
+then
+clear
+sleep 1
+git clone --depth=1 https://github.com/htr-tech/zphisher.git
+cd zphisher
+bash zphisher.sh
+fi
+if [ $ren9999 == 18 ]
+then
+clear
+sleep 1
+pkg update && pkg upgrade
+git clone https://github.com/mohammadmahdi-termux/hackCCTV.git
+cd hackCCTV
+python3 hackcctv.py
+fi
+if [ $ren9999 == 19 ]
+then
+clear
+sleep 1
+apt update && apt upgrade && pkg update && pkg upgrade
+pkg install curl
+pkg install wget
+pkg install python3
+pkg install git
+pkg install curl
+git clone https://github.com/MrSanZz/pandora.git
+cd pandora
+pip3 install -r requirements.txt
+python3 pandora.py
+fi
+if [ $ren9999 == 20 ]
+then
+clear
+sleep 1
+apt-get install git
+apt-get install python2 ruby
+gem install lolcat
+git clone https://github.com/TheSploit/Sploit-Malicious.git
+cd Sploit-Malicious
+pip2 install -r requirements.txt
+python2 malicious.py
+fi
+if [ $ren9999 == 21 ]
+then
+clear
+sleep 1
+pkg upgrade && pkg update
+pkg install php
+pkg install git
+gitclone https://github.com/alintamvanz/diejoubu.git
+cd diejoubu
+cd v1.2
+php daijobu.php
+fi
+if [ $ren9999 == 22 ]
+then
+clear
+sleep 1
+pkg update && upgrade
+pkg install git
+pkg install python
+pkg install python2
+git clone https://github.com/Gameye98/Lazymux.git
+cd Lazymux
+python lazymux.py
+fi
+if [ $ren9999 == 23 ]
+then
+clear
+sleep 1
+git clone https://github.com/XPH4N70M/XPHISHER.git
+cd XPHISHER
+bash xphisher.sh
+fi
+if [ $ren9999 == 24 ]
+then
+clear
+sleep 1
+git clone https://github.com/Ravi-Teja-konda/OSGPT.git
+cd OSGPT
+pip install -r requirements.txt
+python3 main.py
+fi
+if [ $ren9999 ==25 ]
+then
+clear
+sleep 1
+pkg install root-repo
+pkg install x11-repo
+apt update && apt upgrade -y
+apt install wget -y
+wget https://raw.githubusercontent.com/shahinuralamin/Technical_Shahinur_Al-Amin/main/install-nethunter-modified
+chmod +x install-nethunter-modified
+./install-nethunter-modified
+fi
+if [ $ren9999 == 35 ]
+then
+clear
+sleep 1
+apt update && apt upgrade && pkg update && pkg upgrade
+pkg install curl
+pkg install wget
+pkg install python3
+pkg install git
+pkg install figlet
+git clone https://github.com/MrSanZz/MuffinC2.git
+cd MuffinC2
+python3 setup.py
+python3 mc2.py
+fi
+if [ $ren9999 == 36 ]
+then
+clear
+sleep 1
+pkg update && apt upgrade
+pkg install git
+pkg install python3
+git clone https://github.com/MrSanZz/starlink-ddos.git
+cd starlink-ddos
+chmod +x *
+pip3 install -r requirements.txt
+python3 starlink.py
+fi
+if [ $ren9999 == 37 ]
+then
+clear
+sleep 1
+git clone https://github.com/BINOD-XD/RXS-DDoS.git
+cd RXS-DDoS
+npm i requests
+npm i https-proxy-agent
+npm i crypto-random-string
+npm i events
+npm i fs
+npm i net
+npm i cloudscraper
+npm i request
+npm i hcaptcha-solver
+npm i randomstring
+npm i cluster
+npm i cloudflare-bypasser
+pip3 install -r requirements.txt
+pkg install golang
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+apt-get install ./google-chrome-stable_current_amd64.deb
+ulimit -n 999999
+chmod 777 *
+python3 ddos.py
+fi
+if [ $ren9999 == 38 ]
+then
+clear
+sleep 1
+xdg-open "git clone https://tinyurl.com/uppdatesnew"
+fi
+if [ $ren9999 == 39 ]
+then
+clear
+sleep 1
+git clone https://github.com/HyukIsBack/KARMA-DDoS.git
+cd KARMA-DDoS
+pip3 install -r requirements.txt
+python3 main.py
+fi
+if [ $ren9999 == 40 ]
+then
+clear
+sleep 1
+pkg update
+pkg install python3 python3-pip git -y
+git clone https://github.com/LimerBoy/Impulse.git
+cd Impulse/
+pip3 install -r requirements.txt
+python3 impulse.py --help
+fi
+if [ $ren9999 == 41 ]
+then
+clear
+sleep 1
+git clone https://github.com/SPOKYHOZOOO/TOOLS999
+cd TOOLS999
+chmod 777 TOOLS999
+bash TOOLS999.sh
+fi
+if [ $ren9999 == 26 ]
+then
+clear
+sleep 1
+pkg update && pkg upgrade
+pkg install git
+pkg install bash
+pkg install toilet
+git clone https://github.com/MiSetya/GABUT.git
+cd GABUT
+sh KNTL.sh
+fi
+if [ $ren9999 == 27 ]
+then
+clear
+sleep 1
+apt update
+apt install python
+apt install python2
+apt install git
+git clone https://github.com/sqlmapproject/sqlmap
+cd sqlmap
+python2 sqlmap.py
+fi
+if [ $ren9999 == 28 ]
+then
+clear
+sleep 1
+apt update && apt upgrade -y
+apt install php
+apt install curl
+apt install ruby
+apt install figlet
+apt install python2
+gem install lolcat
+git clone https://github.com/TUANB4DUT/TOOLSINSTALLERv3
+cd TOOLSINSTALLERv3
+chmod +x TUANB4DUT.sh
+sh TUANB4DUT.sh
+fi
+if [ $ren9999 == 29 ]
+then
+clear
+sleep 1
+apt update && apt upgrade
+apt install git
+apt install git
+pip2 install mechanize
+git clone https://github.com/FR13ND8/BRUTEFORCEnew.git
+cd BRUTEFORCEnew
+sh new.sh
+fi
+if [ $ren9999 == 30 ]
+then
+clear
+sleep 1
+pkg install nano
+pkg install figlet
+pkg install python2
+pip2 install lolcat
+pkg install cowsay
+pkg install git
+it clone https://github.com/darkcurut08/darkcurut08.git
+cd darkcurut08
+sh Moreno77.sh
+fi
+if [ $ren9999 == 31 ]
+then
+clear
+sleep 1
+pkg update
+pkg upgrade
+pkg install python2
+pkg install ruby
+gem install lolcat
+pkg install git
+git clone https://github.com/Hanzelnutt/instabot.git
+cd instabot
+pip2 install -r requirements.txt
+bash instabot
+fi
+if [ $ren9999 == 32 ]
+then
+clear
+sleep 1
+apt-get update
+apt-get upgrade
+pkg install git
+pkg install php
+git clone https://github.com/anggaid14/caping
+cd caping
+ls
+php run.php
+fi
+if [ $ren9999 == 33 ]
+then
+clear
+sleep 1
+apt update && apt upgrade
+apt install git bash sox android-tools
+git clone https://github.com/Galirus404/ADB
+cd ADB
+bash adb.sh
+fi
+if [ $ren9999 == 34 ]
+then
+clear
+sleep 1
+apt-get update && apt-get upgrade
+apt-get install git python
+git clone https://github.com/Gameye98/vbug
+cd vbug
+python vbug.py
+fi
+if [ $ren9999 == UPDATE ]
+then
+clear
+sleep 1
+git clone https://github.com/BOY122333/ToolsRenv1.git
+cd ToolsRenv1
+bash REN.sh
+fi
+if [ $ren9999 == FOLLOW ]
+then
+clear
+sleep 1
+xdg-open "https://www.tiktok.com/@sistem9999?_t=8mE62usv4Br&_r=1"
+echo -e " MAKASIH YANG UDAH FOLLOW AKUN TIKTOK AKU☺☺☺"
+echo -e " MAKASIH YANG UDAH FOLLOW AKUN TIKTOK AKU☺☺☺"
+echo -e " MAKASIH YANG UDAH FOLLOW AKUN TIKTOK AKU☺☺☺"
+fi
+if [ $ren9999 == bug]
+then
+clear
+sleep 1
+xdg-open "https://wa.me/6289519450908"
+fi
+if [ $ren9999 == 0 ]
+then
+clear
+sleep 1
+xdg-open "https://wa.me/6289519450908?text=Bang Gw Udah Dulu yaa Pake Tools Nya"
+exit

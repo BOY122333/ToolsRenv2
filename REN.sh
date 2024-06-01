a4b2ac7bda6e818b527411c0e5b1c7b04c962162
BOY122333 <pribadisaya10101010@gmail.com>
2024-06-01 11:42:39 +0700

    Update REN.sh

--- REN.sh
+++ REN.sh
@@ -726,9 +726,9 @@ echo -e" menu baru:"
 echo -e "[+]   "
 echo -e "[+]   "
 echo -e "[+]   "
-echo -e"klik enter untuk kembali ke ToolsRenv2 continue
+echo -e"klik enter untuk kembali ke ToolsRenv2 continue"
 fi
-if continue"99 == UPDATE ]
+if [ $ren9999 == UPDATE ]
 then
 clear
 sleep 1
